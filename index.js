5d6d26ab90cef648ad99a5911024e95538470287
tejas-tarte <140580911+tejas-tarte@users.noreply.github.com>
2024-03-16 10:55:39 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var express=require("express");
+var bodyparser=require("body-parser");
+var upload=require("express-fileupload");
+var session=require("express-session");
+
+var user_route=require("./routes/user_route");
+var admin_route=require("./routes/admin_route");
+var app=express();
+app.use(express.static("public/"));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(upload());
+app.use(session({
+    
+    secret:"Tejas Tarte",
+    saveUninitialized:true,
+    resave:true
+
+}));
+app.use("/",user_route);
+app.use("/admin",admin_route);
+
+
+app.listen(1000);
